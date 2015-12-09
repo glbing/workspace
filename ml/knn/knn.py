e0be82565c4f251d676704d611d9176b96d6ce7f
glbing <1207445440@qq.com>
2015-12-09 22:22:14 +0800

    decison_tree

--- ml/knn/knn.py
+++ ml/knn/knn.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 #import operator
 from numpy import *#*代表将numpy全部导入
-import sys
+#import sys
 import matplotlib.pyplot as plt
 def create_dataset():
     group=array([[1.0,1],[1.0,0],[0,0],[0,0.1]])
@@ -18,8 +18,8 @@ print group, label
 else:
     print "456"
 """
-dataset,labels=create_dataset()
-def classify0(inx,k):
+#dataset,labels=create_dataset()
+def classify0(inx,dataset,labels,k):
     datasetsize=dataset.shape[0]
    # print datasetsize
 
@@ -42,9 +42,9 @@ def classify0(inx,k):
    # print classcout
     temp=classcout.items()
     a=sorted(temp,key=lambda temp:temp[1],reverse=True)
-    print a
+    #print a
     return a[0][0]
-'''f __name__=="__main__":
+'''if __name__=="__main__":
     if len(sys.argv)!=2:
         print "input error"
     else:
@@ -68,21 +68,56 @@ def file_matrix(filename):
    # print mat
    # print len(label_vector)
     return mat,label_vector
-mat,label=file_matrix("datingTestSet.txt") 
-for i in range(len(label)):
-    if label[i] == "largeDoses":
-        label[i]=int(1)
-    elif label[i] =="smallDoses":
-        label[i]=int(2)
-    else:
-        label[i]=int(3)
-label=array(label)
-fig=plt.figure()
-ax=fig.add_subplot(111)
-ax.set_title("dataset")
-ax.scatter(mat[:,0],mat[:,1],15*label,15*label)
-plt.show()
+#mat,label=file_matrix("datingTestSet.txt") 
 
+
+def autonorm(mat_list):
+    #dataset=mat_list[0]
+    colum_min=mat_list[0].min(0)
+    colum_max=mat_list[0].max(0)
+    colum_min1=tile(colum_min,(mat_list[0].shape[0],1))
+    colum_max1=tile(colum_max,(mat_list[0].shape[0],1))
+    range=colum_max1-colum_min1
+    mat_list[0]=(mat_list[0]-colum_min1)/range
+   # return dataset
+#print mat
+
+
+
+def knn_test():
+    ratio=0.1
+    datamat,labels=file_matrix("datingTestSet.txt")
+    for i in range(len(labels)):
+        if labels[i] == "largeDoses":
+            labels[i]=int(1)
+        elif labels[i] =="smallDoses":
+            labels[i]=int(2)
+        else:
+            labels[i]=int(3)
+    mat_list=[]
+    mat_list.append(datamat)
+    autonorm(mat_list)
+    mat=mat_list[0]
+    labels=array(labels)
+
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    ax.set_title("dataset")
+    ax.scatter(mat[:,0],mat[:,1],15*labels,15*labels)
+    plt.show()
+
+    total_num=mat.shape[0]
+    test_num=int(total_num*ratio)
+    j=0
+    for i in range(test_num):
+        result=classify0(mat[i,:],mat[test_num:total_num,:],labels[test_num:total_num],7)
+        if result == labels[i]:
+            j += 1.0
+            print (str(result)+ " "+ str(labels[i]))
+    print (j/float(test_num))
+
+if __name__ == "__main__":
+    knn_test()
 
 
 
